942e2eeed17be5163aa389e746e8a20744eee22f
Roman Severyn <romaseverin50@gmail.com>
2023-02-14 18:54:01 +0200

    add home screen

---
+++ src/screen/Home.js
@@ -0,0 +1,13 @@
+import { View } from 'native-base';
+import { Text } from 'react-native';
+
+export const Home = () => {
+  var aa = 1;
+  return (
+    <View flex={1} alignItems={'center'} justifyContent={'center'}>
+      <Text style={{ backgroundColor: 'red', marginRight: 5, fontSize: 24 }}>
+        Welcome Home
+      </Text>
+    </View>
+  );
+};
