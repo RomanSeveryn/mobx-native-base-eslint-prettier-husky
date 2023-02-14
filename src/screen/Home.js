3cf060c5cacb5c83a148cd59dfac6991234d7ee9
Roman Severyn <romaseverin50@gmail.com>
2023-02-14 18:56:57 +0200

    test

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -3,6 +3,7 @@ import { Text } from 'react-native';
 
 export const Home = () => {
   var aa = 1;
+  var ab = 1;
   return (
     <View flex={1} alignItems={'center'} justifyContent={'center'}>
       <Text style={{ backgroundColor: 'red', marginRight: 5, fontSize: 24 }}>
